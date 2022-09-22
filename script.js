5b9afbaba0f62104ef56b184536b6447bb52b5d5
ashxtt <111319560+ashxtt@users.noreply.github.com>
2022-09-21 20:00:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,36 @@ const game = {
       this.secretNum =
         Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) +
         this.smallestNum
+
         let guess;
         while(this.guess !== this.secretNum){
-        
+            guess = this.getGuess();
+            this.prevGuesses.push(guess);
+            this.render(guess);
+            if(guess === this.secretNum){
+                return;
+            }
         }
     },
     getGuess: function(){
         let getGuess = parseInt(prompt("Enter a guess between 1 - 100"))
         console.log(this.prevGuesses.push(this.getGuess));
-        return
-        if(getGuess === secretNum){
-            alert("You are right!")
-        }else if(getGuess > secretNum){
-            alert("You are wrong! Try again")
+        return guess;
+        
+    },
+    render: function(guess){
+        if(guess > this.secretNum){
+            alert('Your guess is too high')
+        }else if(guess < this.secretNum){
+            alert('Your guess is too low')
+        }else if(guess === this.secretNum){
+            alert(`congrats! You guessed the number in ${this.prevGuesses.length} Guesses!`)
         }
+
     }
 
 
   }
+game.play()
+  console.log(game.getGuess())
 
