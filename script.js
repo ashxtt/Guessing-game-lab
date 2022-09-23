61c04f4ffe24c8e638a7afb43257b82158c6df20
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-09-23 15:52:40 -0600

    add feedback

--- script.js
+++ script.js
@@ -10,21 +10,31 @@ const game = {
         this.smallestNum
 
         let guess;
-        while(this.guess !== this.secretNum){
+        //guess is not a property of object so your while loop should only use guess not this.guess
+        while(guess !== this.secretNum){
             guess = this.getGuess();
-            this.prevGuesses.push(guess);
+            //i used the push in getGuess() so I commented this out
+            //this.prevGuesses.push(guess);
             this.render(guess);
             if(guess === this.secretNum){
                 return;
             }
+            //break the loop if the user hits cancel
+            if(!guess)break
         }
     },
     getGuess: function(){
         let getGuess = parseInt(prompt("Enter a guess between 1 - 100"))
-        console.log(this.prevGuesses.push(this.getGuess));
-        return guess;
+        //instead of console.log() I changed it to the below using the getGuess variable above
+        this.prevGuesses.push(getGuess)
+        //console.log(this.prevGuesses.push(this.getGuess));
+
+        //you need to return getGuess because that is what you named it on line 23
+        return getGuess;
         
     },
+
+    // your render function looks great doing exactly what render should do with a parameter passed to it
     render: function(guess){
         if(guess > this.secretNum){
             alert('Your guess is too high')
@@ -39,5 +49,5 @@ const game = {
 
   }
 game.play()
-  console.log(game.getGuess())
+  //console.log(game.getGuess())
 
